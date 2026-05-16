4fdf704ea969f3ea77e2947e575bc70eec4def36
TranViet04 <tranquocviet18102004@gmail.com>
2026-05-16 15:07:11 +0700

    [add] lab-01 ex01 ex02

---
+++ Lab-01/ex02/ex02_01.py
@@ -0,0 +1,6 @@
+# Nhập tên và tuổi người dùng
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+# In ra thông tin người dùng
+print("Tên của bạn là:", ten)
+print("Tuổi của bạn là:", tuoi)
---
+++ Lab-01/ex02/ex02_02.py
@@ -0,0 +1,6 @@
+#NhậP bán kính từ bàn phím
+ban_kinh = float(input("Nhập bán kính hình tròn: "))
+#Tính diện tich hình tròn
+dien_tich = 3.14 * ban_kinh ** 2
+#In ra diện tích hình tròn
+print("Diện tích hình tròn là:", dien_tich)
